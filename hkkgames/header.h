47327dd9c894faf7359ed205f4e647c3c8fa386b
juzixiaoren <a2236482268@outlook.com>
2024-04-15 23:38:21 +0800

    小小写了点东西

---
+++ hkkgames/header.h
@@ -0,0 +1,11 @@
+#pragma once
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <ctime>
+#include <cstdlib>
+#include <windows.h>
+#ifndef HEADER_H
+#define HEADER_H
+#endif
